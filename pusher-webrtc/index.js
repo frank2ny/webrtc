55c51f82ca3f78e066a7941f7dcf0ec5544a6065
frank2ny <frank2nyyy@gmail.com>
2020-03-17 15:25:04 +0300

    creating the landing page

--- pusher-webrtc/index.js
+++ pusher-webrtc/index.js
@@ -26,3 +26,16 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     return console.log('Server is up on 3000')
 });
+
+// get authentictation for the channel;
+app.post("/pusher/auth", (req, res) => {
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    var presenceData = {
+        user_id: Math.random()
+            .toString(36)
+            .slice(2) + Date.now()
+    };
+    const auth = pusher.authenticate(socketId, channel, presenceData);
+    res.send(auth);
+});
